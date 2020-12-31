b72402d42131549bc5d9f5895c17ae2fdf3b9d31
Nathaniel Birne <nbirne71@gmail.com>
2020-12-31 15:14:17 -0500

    Add new color options

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// Container will be 960 pixels on each side
-const CONTAINERSIZE = 960;
+// Container will be this many pixels on each side
+const CONTAINERSIZE = 600;
+let color = "#495057";
 
 function createGrid(width) {
     let container = document.querySelector("#container");
     let boxWidth = CONTAINERSIZE / width;
-    console.log(boxWidth);
+    
+    // Gives container a fixed width so that boxes won't wrap to fit in window
+    container.style.width = `${CONTAINERSIZE}px`;
+
     for (let i = 0; i < width; i++) {
         for (let j = 0; j < width; j++) {
             let newBox = document.createElement("div");
@@ -21,16 +25,18 @@ function colorBox() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach(function(box) {
         box.addEventListener("mouseenter", e => {
-            let randomColor = Math.floor(Math.random()*16777215).toString(16);
-            console.log(randomColor);
-            e.target.style.backgroundColor = "#" + randomColor;
+            if (color === "Rainbow") {
+                randomColor = Math.floor(Math.random()*16777215).toString(16);
+                e.target.style.backgroundColor = "#" + randomColor;
+            }
+            else e.target.style.backgroundColor = color;
         });
     });
 }
 
 function clear() {
-    let button = document.querySelector("button");
-    button.addEventListener("click", function(e) {
+    let clearButton = document.querySelector("#clear");
+    clearButton.addEventListener("click", function(e) {
         // Prompt user for width of new grid
         let width = getWidth();
 
@@ -51,6 +57,26 @@ function getWidth() {
     return width;
 }
 
+function chooseColor() {
+    // Set up event listeners for when each button is clicked
+    let colorButtons = document.querySelectorAll(".colorButton");
+    colorButtons.forEach(function(button) {
+        button.addEventListener("click", function(e) {
+            let colorChoice = e.target.textContent;
+
+            if (colorChoice === "Dark") color = "#495057";
+            else if (colorChoice === "Erase") color = "#DEE2E6";
+            else if (colorChoice === "Rainbow") color = "Rainbow";
+        });
+    });
+    
+    let customColor = document.querySelector("#customColor");
+    customColor.addEventListener("input", function(e) {
+        color = e.target.value;
+    });
+}
+
 createGrid(16);
 colorBox()
 clear();
+chooseColor();
