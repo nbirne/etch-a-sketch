164bf46a93ee37e5ea79ae131b8c16516bf8ccbd
Nathaniel Birne <nbirne71@gmail.com>
2021-01-03 19:04:59 -0500

    Finalize project

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Container will be this many pixels on each side
-const CONTAINERSIZE = 600;
-let color = "#495057";
+const CONTAINERSIZE = 500;
+let color = "#5D737E";
+let colorOn = true;
 
-function createGrid(width) {
+function createGrid() {
+    let slider = document.querySelector("#slider");
+    let width = slider.value;
     let container = document.querySelector("#container");
+
+    // Set boxWidth so that boxes always add up to fill entire container
     let boxWidth = CONTAINERSIZE / width;
     
     // Gives container a fixed width so that boxes won't wrap to fit in window
@@ -21,32 +26,37 @@ function createGrid(width) {
     }
 }
 
+function toggleColor() {
+    let container = document.querySelector("#container");
+    container.addEventListener("click", function() {
+        colorOn ? (colorOn = false) : (colorOn = true);
+    });
+}
+
 function colorBox() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach(function(box) {
-        box.addEventListener("mouseenter", e => {
-            if (color === "Rainbow") {
-                randomColor = Math.floor(Math.random()*16777215).toString(16);
-                e.target.style.backgroundColor = "#" + randomColor;
+        box.addEventListener("mouseenter", function(e) {
+            if (colorOn === true) {
+                if (color === "Rainbow") {
+                    randomColor = Math.floor(Math.random()*16777215).toString(16);
+                    e.target.style.backgroundColor = "#" + randomColor;
+                }
+                else e.target.style.backgroundColor = color;
             }
-            else e.target.style.backgroundColor = color;
         });
     });
 }
 
 function clear() {
     let clearButton = document.querySelector("#clear");
+    let containerColor = document.querySelector("#container").style.backgroundColor;
     clearButton.addEventListener("click", function(e) {
-        // Prompt user for width of new grid
-        let width = getWidth();
-
         // Remove current grid
         let boxes = document.querySelectorAll(".box");
-        boxes.forEach(box => box.remove());
+        boxes.forEach(box => box.style.backgroundColor = containerColor);
 
-        // Create new grid
-        createGrid(width);
-        colorBox();
+        colorOn = true;
     });
 }
 
@@ -64,19 +74,49 @@ function chooseColor() {
         button.addEventListener("click", function(e) {
             let colorChoice = e.target.textContent;
 
-            if (colorChoice === "Dark") color = "#495057";
-            else if (colorChoice === "Erase") color = "#DEE2E6";
+            if (colorChoice === "Dark") color = "#5D737E";
+            else if (colorChoice === "Erase") color = "#C0FDFB";
             else if (colorChoice === "Rainbow") color = "Rainbow";
         });
     });
     
+    // Set up event listeners to store custom colors
     let customColor = document.querySelector("#customColor");
     customColor.addEventListener("input", function(e) {
         color = e.target.value;
     });
+    customColor.addEventListener("click", function(e) {
+        color = e.target.value;
+    });
+
+    // Give only the chosen color option a dark border
+    let colorSelectors = document.querySelectorAll(".colorSelector");
+    colorSelectors.forEach(function(selector) {
+        selector.addEventListener("click", function(e) {
+            colorSelectors.forEach(button => button.style.border = "1px solid #C0FDFB");
+            e.target.style.border = "1px solid #5D737E";
+        });
+    });
 }
 
-createGrid(16);
+function changeSize() {
+    // Set up event listener to resize container when slider is used
+    slider.addEventListener("change", function(e) {
+        // Remove current grid
+        let boxes = document.querySelectorAll(".box");
+        boxes.forEach(box => box.remove());
+
+        // Create new grid
+        createGrid();
+        colorBox();
+
+        colorOn = true;
+    });
+}
+
+createGrid();
 colorBox()
 clear();
 chooseColor();
+changeSize();
+toggleColor();
